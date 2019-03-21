2e465e62f2ce4b43ac75c2f17f7daf1c089f6ffa
Shivam Tayal <shitaya1@in.ibm.com>
2019-03-21 15:35:02 +0530

    Sprinh HelooWorld File

---
+++ Labs/SpringCore/SpringHelloWorld/Greetings.java
@@ -0,0 +1,14 @@
+package com.example.spring.core;
+
+public class Greetings {
+
+	private String message = "Spring Hello World!!";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
